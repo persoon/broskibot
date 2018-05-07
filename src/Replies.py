83b35969f5d6869b6fabee1d1347cfe5288cd132
Aaron Hall <aaronhallx@gmail.com>
2018-05-06 22:40:34 -0700

    added dnd rolls

--- src/Replies.py
+++ src/Replies.py
@@ -19,16 +19,32 @@ class Replies:
         # Say hi
         if message.content.startswith("!hello"):
             # say hi back, tagging the user
-            str = "Hello {0.author.mention}"
-            return [str]
+            rat = "Hello {0.author.mention}"
+            return [rat]
         elif message.content.startswith("!roulette"):
-            str = ["A gun is placed to {0.author.mention}'s head."]
+            rat = ["A gun is placed to {0.author.mention}'s head."]
             num = random.randint(1, 6)
             if num == 4:
-                str.append("The trigger is pulled and {0.author.mention} lies dead in the chat.")
+                rat.append("The trigger is pulled and {0.author.mention} lies dead in the chat.")
             else:
-                str.append("The gun clicks and {0.author.mention} lives to see another day.")
-            return str
+                rat.append("The gun clicks and {0.author.mention} lives to see another day.")
+            return rat
+        elif message.content.startswith("!roll"):
+            # !roll 321d123
+            msg = message.content
+            str_split = msg.split()
+            dice = str_split[1]
+            left, right = dice.split("d")
+            total = 0
+            val = "("
+            for i in range(int(left)):
+                rng = random.randint(1, int(right))
+                val += str(rng)
+                if i < int(left) - 1:
+                    val += ","
+                total += rng
+            reply = val + ") Total: " + str(total)
+            return [reply]
         else:
             # return -1 if we don't need to reply to the message
             print("It doesn't look like anything to me..")
