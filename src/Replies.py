d09e2b7eb5c9f71e89bba2f2f29d57663ad33194
Andrew <andrewpcoles@gmail.com>
2018-06-01 12:04:57 -0500

    Added trump predictive text function in replies

--- src/Replies.py
+++ src/Replies.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from src import Parameters
 
 # get the borg client from broskibot
@@ -53,6 +54,41 @@ class Replies:
                     "Cannot predict now", "Concentrate and ask again", "Don't count on it",
                     "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
             return [random.choice(coke)]
+
+        elif message.content.startswith("!trump"):
+            trump = open('../text-files/speeches.txt', encoding='utf8').read()
+
+            corpus = trump.split()
+
+            def make_pairs(corpus):
+                for i in range(len(corpus) - 1):
+                    yield (corpus[i], corpus[i + 1])
+
+            pairs = make_pairs(corpus)
+
+            word_dict = {}
+
+            for word_1, word_2 in pairs:
+                if word_1 in word_dict.keys():
+                    word_dict[word_1].append(word_2)
+                else:
+                    word_dict[word_1] = [word_2]
+
+            first_word = np.random.choice(corpus)
+
+            while first_word.islower():
+                first_word = np.random.choice(corpus)
+
+            chain = [first_word]
+
+            n_words = 5
+
+            for i in range(n_words):
+                chain.append(np.random.choice(word_dict[chain[-1]]))
+
+            speech = ' '.join(chain)
+            return [speech]
+
         else:
             print("It doesn't look like anything to me..")
             return [None]
---
+++ src/Trump-chat.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+trump = open('../private/speeches.txt', encoding='utf8').read()
+
+corpus = trump.split()
+
+
+def make_pairs(corpus):
+    for i in range(len(corpus) - 1):
+        yield (corpus[i], corpus[i + 1])
+
+
+pairs = make_pairs(corpus)
+
+word_dict = {}
+
+for word_1, word_2 in pairs:
+    if word_1 in word_dict.keys():
+        word_dict[word_1].append(word_2)
+    else:
+        word_dict[word_1] = [word_2]
+
+first_word = np.random.choice(corpus)
+
+while first_word.islower():
+    first_word = np.random.choice(corpus)
+
+chain = [first_word]
+
+n_words = 50
+
+for i in range(n_words):
+    chain.append(np.random.choice(word_dict[chain[-1]]))
+
+speech = ' '.join(chain)
